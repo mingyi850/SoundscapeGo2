41f5340d34747c22d1f1795bec40b072100ff687
Vlad Repinskiy <editedity@gmail.com>
2019-01-08 18:58:16 +0000

    Basic scripts added, created an interactable destination button prefab.

---
+++ Assets/SampleButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour
+{
+    public Button button;
+    public Text nameLabel;
+    public Text distanceLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Setup()
+    {
+
+    }
+}
---
+++ Assets/ScrollList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Destination
+{
+    public string destinationName;
+    public float longitude;
+    public float latitude;
+}
+
+public class ScrollList : MonoBehaviour
+{
+    public List<Destination> destinationList;
+    public Transform contentPanel;
+    public SimpleObjectPool buttonObjectPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AddButtons()
+    {
+        for (int i = 0; i < destinationList.Count; i++)
+        {
+            Destination destination = destinationList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+        }
+    }
+}
