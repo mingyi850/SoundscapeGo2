70d7798a27538e8983a225523eb0fe98bafa940c
Vlad Repinskiy <editedity@gmail.com>
2019-02-13 16:51:40 +0000

    Side Menu first draft

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+	public RectTransform sideMenu;
+
+	public void ChangeToHomeScene() {
+		SceneManager.LoadScene ("Destination Select");
+	}
+
+	public void SideMenuSlideOut() {
+		sideMenu.DOAnchorPos(new Vector2(-200, 0), 0.25f);
+	}
+}
